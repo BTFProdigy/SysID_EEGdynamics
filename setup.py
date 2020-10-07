f7ef1b90f322f5f01658aa53c94d94f47a86a2f6
tdgriffith <griffith@iastate.edu>
2020-10-07 14:15:16 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repo is the implementation of our paper "System Identification Methods for Dyanmic Models of Brain Activity".',
+    author='Morpheus Lab',
+    license='MIT',
+)
